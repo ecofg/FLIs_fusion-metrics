4824530a737e2b27021c66233e5f4761da19cc73
Fu <69113936+ecofg@users.noreply.github.com>
2020-12-23 16:04:19 +0800

    Update information  volume by moving window method.py

--- information  volume by moving window method.py
+++ information  volume by moving window method.py
@@ -26,27 +26,26 @@ sds=4#four significant digits after the decimal point
 arcpy.CheckOutExtension("spatial")
 arcpy.env.parallProcessingFactor = "50%"
 arcpy.gp.overwriteOutput = True
-inRas = Raster(inputpath)  # 提取信息，作为背景信息。
-desc = arcpy.Describe(inRas)  # 描述函数
-extent1 = desc.extent  # 使用提取矩形的四个顶点坐标
+inRas = Raster(inputpath)
+desc = arcpy.Describe(inRas)
+extent1 = desc.extent
 print(extent1)
-arcpy.env.extent = extent1  # 环境设置范围，保证所有的栅格处理范围一致，不会发生偏移。
-sourceSR = arcpy.Describe(inRas).spatialReference  # 获取原坐标系
-arcpy.env.outputCoordinateSystem = sourceSR  # 默认输出坐标系
-lowerLeft = arcpy.Point(inRas.extent.XMin, inRas.extent.YMin)  # 左下顶点坐标，为了定位使用。
+arcpy.env.extent = extent1
+sourceSR = arcpy.Describe(inRas).spatialReference
+arcpy.env.outputCoordinateSystem = sourceSR
+lowerLeft = arcpy.Point(inRas.extent.XMin, inRas.extent.YMin)
 cellSize = inRas.meanCellWidth
 print(lowerLeft, cellSize)
 gc.set_threshold(100, 5, 5)
 
 weishu=range(1,sds+1)
-chengshu=map(lambda x:pow(10,x),weishu)#需要验证的小数位 10000对应小数后4位
+chengshu=map(lambda x:pow(10,x),weishu)
 
-# 创建全为空值的矩形数据帧, background window
+# background window
 dfno = pd.DataFrame(index=range(2 * r + 1), columns=range(2 * r + 1))
-# 在建立相应的圆形数据帧
 for i in range(2 * r + 1):
     for n in range(2 * r + 1):
-        if pow(r - i, 2) + pow(r - n, 2) <= pow(r, 2):  # 圆形
+        if pow(r - i, 2) + pow(r - n, 2) <= pow(r, 2):
             dfno.iat[i, n] = 1
 class Node:
     def __init__(self, freq):
@@ -55,24 +54,21 @@ class Node:
         self.father = None
         self.freq = freq
 
-    def __repr__(self):  # 显示属性#change the string representation of instances
+    def __repr__(self):
         return "Node({0.freq!r})".format(self)
 
     def isLeft(self):
         return self.father.left == self
-
-
-# 创建叶子节点  create nodes
 def creatNodes(freqs):
     return [Node(freq) for freq in freqs]
 
 
-# creat Huffman-tree 创建Huffmans树
+# creat Huffman-tree
 def createHuffmanTree(nodes):
-    queue = nodes[:]  # copy of the nodes
+    queue = nodes[:] 
     while len(queue) > 1:
-        queue.sort(key=lambda item: item.freq)  # sort the objects by certain attribute
-        node_left = queue.pop(0)  # 移除第一个元素
+        queue.sort(key=lambda item: item.freq) 
+        node_left = queue.pop(0)
         node_right = queue.pop(0)
         node_father = Node(node_left.freq + node_right.freq)
         node_father.left = node_left
@@ -80,13 +76,13 @@ def createHuffmanTree(nodes):
         node_left.father = node_father
         node_right.father = node_father
         queue.append(node_father)
-    queue[0].father = None  # self.father = None ,可省略
+    queue[0].father = None 
     return queue[0]  # root node
 
 
-# Huffman 编码
+# Huffman 
 def huffmanEncoding(nodes, root):
-    codes = [""] * len(nodes)  # 叶子节点数即为编码数
+    codes = [""] * len(nodes)
     for i in range(len(nodes)):
         node_tmp = nodes[i]
         while node_tmp != root:
@@ -97,7 +93,7 @@ def huffmanEncoding(nodes, root):
             node_tmp = node_tmp.father
     return codes
 if __name__ == '__main__':
-    # 归一化处理
+    # Normalization progress
     raster = inRas
     maxValue = raster.maximum
     print"maxvalue:" + str(maxValue)
@@ -106,43 +102,40 @@ if __name__ == '__main__':
     norraster = (raster - float(minValue)) / (float(maxValue) - float(minValue))
     del raster, gc.garbage[:]
     gc.collect()
-    # 转为pd
     numraster = arcpy.RasterToNumPyArray(norraster)
     dfraster = pd.DataFrame(numraster)
     del norraster, numraster
-    hang = dfraster.shape[0]  # 行数
-    lie = dfraster.shape[1]  # 列数
-    dfzero = pd.DataFrame(np.zeros((hang, lie)))  # 创建所有值为0的数据帧,作为填充数据背景
-    # 创建圆形移动窗口
+    hang = dfraster.shape[0]
+    lie = dfraster.shape[1]
+    dfzero = pd.DataFrame(np.zeros((hang, lie)))
+    # moving window
     n = 0
     for l in range(lie - 2 * r):
         print("The progress rate is %3f"%(l/(lie-2*r)))
         for h in range(hang - 2 * r):
-            n += 1  # 计数起点，矩形框的左上角
+            n += 1 
             # print ("No. is "+ str(n))
             dfclp = dfraster.iloc[h:h + 2 * r + 1,l:l + 2 * r + 1]
-            # 更改行列号，重置，使其可以运算
             dfwindow = dfclp.rename(index=dict(map(lambda x, y: [x, y], dfclp.index.tolist(), dfno.index.tolist())),columns=dict(map(lambda x, y: [x, y], dfclp.columns.tolist(), dfno.columns.tolist())))
             dfcomp = dfno * dfwindow
-            arrcomp = np.array(dfcomp).flatten().astype(float)  # 先转一维
-            arrready = arrcomp[-np.isnan(arrcomp)]  # 删除空值
+            arrcomp = np.array(dfcomp).flatten().astype(float)
+            arrready = arrcomp[-np.isnan(arrcomp)] 
             # print(arrready)
             del arrcomp, dfcomp, dfwindow, dfclp
-            # 准备进入计算过程，计算信息量，0.5系数法
             xinxiliang = 0
-            for wei, cheng in zip(weishu, chengshu):  # 相应的小数位处理
+            for wei, cheng in zip(weishu, chengshu):
                 # print("wei is %d" % wei)
                 # print("cheng is %d" % cheng)
-                rastercheng = np.rint(np.around(arrready, decimals=wei) * cheng)  # 转为对应的整值
+                rastercheng = np.rint(np.around(arrready, decimals=wei) * cheng)
                 rasterlist = rastercheng.tolist()
                 del rastercheng, gc.garbage[:]
                 gc.collect()
                 changdu = len(rasterlist)
                 # print(changdu)
-                listcount = Counter(rasterlist)  # 得到计数的字典
+                listcount = Counter(rasterlist)
                 del rasterlist, gc.garbage[:]
                 gc.collect()
-                # 最优编码信息量统计
+                # information volume
                 chars = listcount.keys()
                 freqs = listcount.values()
                 chars_freqs = zip(chars, freqs)
@@ -153,21 +146,17 @@ if __name__ == '__main__':
                 chars_codes.sort(key=lambda item: item[0][1])  # sort the result by the freqs
                 sum = 0
                 for item in chars_codes:
-                    # print('Character:%s freq:%-2d encoding:%s' % (item[0][0], item[0][1], item[1]))  # 得到的编码为字符串，可以转数字（十进制），再根据（位数-1）*2+..等转为二进制。
-                    # 直接len(item[1])就是其对应的二进制编码的长度，也是其占用的二进制字长，一个二进制位包含的信息为一个比特。所以其长度就是比特数。
-                    sum += item[0][1] * len(item[1])  # 只要总结果
-                # 权重二分法，分母取其之和。
+                    # print('Character:%s freq:%-2d encoding:%s' % (item[0][0], item[0][1], item[1])) 
+                    sum += item[0][1] * len(item[1]) 
                 xinxiliang += int(math.pow(0.5, wei) / (1 - math.pow(0.5, weishu[-1])) * sum)
             # print("xinxiliang is %f"%xinxiliang)
-            # 中新赋值 为 （d+r,d+r),，没有+1，注意语法
             dfzero.iat[h + r, l + r] = xinxiliang
             del arrready, gc.garbage[:]
     arrayzero = np.array(dfzero)
     del dfzero, dfraster, gc.garbage[:]
     newRaster = arcpy.NumPyArrayToRaster(arrayzero, lowerLeft, cellSize)
     print("newRaster is ok")
-    # 定义投影则是原来坐标系无或者unknown
-    arcpy.DefineProjection_management(newRaster, sourceSR)  # 无坐标系需要定义
+    arcpy.DefineProjection_management(newRaster, sourceSR)
     newRaster.save(outpath)
     print ("saving is ok")
     del newRaster, arrayzero, gc.garbage[:]
